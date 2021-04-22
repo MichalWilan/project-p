02f1a54d2dd64dbaa6816a58b8eaf6dd01f55d8d
Michal Wilanowski <wilanowski.michal@gmail.com>
2021-04-22 16:08:30 +0200

    change in script.ks

--- src/js/script.js
+++ src/js/script.js
@@ -53,13 +53,44 @@
   };
 
   class Product {
-    constructor() {
+    constructor(id, data) {
       const thisProduct = this;
+      thisProduct.id = id;
+      thisProduct.data = data;
+
+      thisProduct.renderInMenu();
+
       console.log('New Product:', thisProduct);
+    }
+    renderInMenu() {
+      const thisProduct = this;
+      /* generate HTML based on templates */
+      const generatedHTML = templates.menuProduct(thisProduct.data);
+      /* create element using utils.createElementFromHTML */
+      thisProduct.element = utils.createDOMFromHTML(generatedHTML);
+      /* find menu container */
+      const menuContainer = document.querySelector(select.containerOf.menu);
+      /* add element to menu */
+      menuContainer.appendChild(thisProduct.element);
     }
   }
 
   const app = {
+    initMenu: function () {
+      const thisApp = this;
+      // console.log('thisApp.data:' thisApp.data);
+
+      for (let productData in thisApp.data.products) {
+        new Product(productData, thisApp.data.products[productData]);
+      }
+    },
+
+    initData: function () {
+      const thisApp = this;
+
+      thisApp.data = dataSource;
+    },
+
     init: function () {
       const thisApp = this;
       console.log('*** App starting ***');
@@ -67,15 +98,11 @@
       console.log('classNames:', classNames);
       console.log('settings:', settings);
       console.log('templates:', templates);
+
+      thisApp.initData();
+      thisApp.initMenu();
     },
   };
-  app.initMenu();
-  {
-    const testProduct = new Product();
-    console.log('testProduct:', testProduct);
-  }
+
   app.init();
-  {
-    this.app.initMenu();
-  }
 }
